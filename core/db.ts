27845876b781ff52fa344c3901ee98be6829df6f
NikitaDe <69841196+NikitaDe@users.noreply.github.com>
2020-12-01 21:29:34 +0300

    dot

--- core/db.ts
+++ core/db.ts
@@ -1,16 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/focus',{
-  useNewUrlParser:true,
-  useCreateIndex:true,
-  useUnifiedTopology:true,
-  useFindAndModify:false,
-});
+mongoose.connect(
+  process.env.MONGODB_URL ||
+    // 'mongodb://127.0.0.1:27017/focus' ||
+    'mongodb+srv://UserAdm:kodfg123@cluster0.3ydia.mongodb.net/focus?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+);
 
-const db =mongoose.connection;
+const db = mongoose.connection;
 
-db.on('error', console.error.bind(console,"CONNECTION ERROR:"));
+db.on('error', console.error.bind(console, 'CONNECTION ERROR:'));
 
-export {db, mongoose}
+export { db, mongoose };
